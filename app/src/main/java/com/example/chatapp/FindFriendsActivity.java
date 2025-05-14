bc6ca7a299a2d0c823c8d7e7b73d0308842f5468
Axel Alvarez <axjavier4alvarez44@gmail.com>
2025-05-14 16:58:43 -0600

    cambios

---
+++ app/src/main/java/com/example/chatapp/FindFriendsActivity.java
@@ -0,0 +1,40 @@
+package com.example.chatapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FindFriendsActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private RecyclerView FindFriendsRecyclerList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_find_friends);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mToolbar = (Toolbar) findViewById(R.id.find_friends_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Find Friends");
+
+        FindFriendsRecyclerList = (RecyclerView) findViewById(R.id.find_friends_recycler_list);
+        FindFriendsRecyclerList.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
