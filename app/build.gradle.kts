bcd32093471414f4dba504c9a4d6b791966402c6
Axel Alvarez <axjavier4alvarez44@gmail.com>
2025-05-14 19:14:19 -0600

    cambios del video 1 al 30

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+ plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
+}
+
+android {
+    namespace = "com.example.chatapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.chatapp"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
+    implementation(libs.firebase.database)
+    implementation("de.hdodenhof:circleimageview:2.2.0")
+    implementation(libs.firebase.storage)
+    implementation ("com.firebaseui:firebase-ui-database:9.0.0")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation("com.github.CanHub:Android-Image-Cropper:4.4.0")
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
+
+    implementation ("com.squareup.picasso:picasso:2.71828")
+
+}
---
+++ app/src/main/java/com/example/chatapp/FindFriendsActivity.java
@@ -0,0 +1,109 @@
+package com.example.chatapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import com.squareup.picasso.Picasso;
+
+
+public class FindFriendsActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private RecyclerView FindFriendsRecyclerList;
+    private DatabaseReference UsersRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_find_friends);
+
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mToolbar = (Toolbar) findViewById(R.id.find_friends_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        FindFriendsRecyclerList = (RecyclerView) findViewById(R.id.find_friends_recycler_list);
+        FindFriendsRecyclerList.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options =
+                new FirebaseRecyclerOptions.Builder<Contacts>()
+                        .setQuery(UsersRef, Contacts.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull FindFriendViewHolder holder, int position, @NonNull Contacts model) {
+                        holder.userName.setText(model.getName());
+                        holder.userStatus.setText(model.getStatus());
+                        Picasso.get().load(model.getImage()).into(holder.profileImage);
+                    }
+
+                    @NonNull
+                    @Override
+                    public FindFriendViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from(parent.getContext())
+                                .inflate(R.layout.users_display_layout, parent, false);
+                        return new FindFriendViewHolder(view);
+                    }
+                };
+
+        FindFriendsRecyclerList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+
+
+
+    public static class FindFriendViewHolder extends RecyclerView.ViewHolder
+    {
+
+        TextView userName, userStatus;
+        CircleImageView profileImage;
+
+        public FindFriendViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.users_profile_name);
+            userStatus = itemView.findViewById(R.id.user_status);
+            profileImage = itemView.findViewById(R.id.users_profile_image);
+
+        }
+    }
+}
+
